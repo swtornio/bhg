8ac1996d353105905cf8e45a019d795f28efcb84
swtornio <swtornio@gmail.com>
2020-10-05 06:30:12 -0500

    more chapter 2

---
+++ ch-2/io-example/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+// FooReader defines an io.Reader to read from stdin
+type FooReader struct{}
+
+// Read reads data drom stdin
+func (fooReader *FooReader) Read(b []byte) (int, error) {
+	fmt.Print("in > ")
+	return os.Stdin.Read(b)
+}
+
+// FooWriter defines an io.Writer to write to stdout
+type FooWriter struct{}
+
+// Write writes data to Stdout
+func (fooWriter *FooWriter) Write(b []byte) (int, error) {
+	fmt.Print("out> ")
+	return os.Stdout.Write(b)
+}
+
+func main() {
+	// Instantiate reader and writer
+	var (
+		reader FooReader
+		writer FooWriter
+	)
+
+	// Create buffer to hold input/output
+	input := make([]byte, 4096)
+
+	// use reader to read input
+	s, err := reader.Read(input)
+	if err != nil {
+		log.Fatalln("Unable to read data")
+	}
+	fmt.Printf("Read %d bytes from stdin\n", s)
+
+	// use writer to write output
+	s, err = writer.Write(input)
+	if err != nil {
+		log.Fatalln("Unable to write data")
+	}
+	fmt.Printf("Wrote %d bytes to stdout\n", s)
+}
